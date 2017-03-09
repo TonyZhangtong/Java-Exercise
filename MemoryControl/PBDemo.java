5bb6b4a867f5479cf5ad194ea2c4409a3998b069
South <tony.zhangtong@outlook.com>
2017-03-09 16:28:54 -0500

    use ProcessBuilder to start a new process

---
+++ MemoryControl/PBDemo.java
@@ -0,0 +1,15 @@
+package org.totalbeginner;
+
+public class PBDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			ProcessBuilder proc = new ProcessBuilder("notepad.exe", "testfile");
+			proc.start();
+		} catch (Exception e) {
+			System.out.println("Error executing notepad.");
+		}
+	}
+
+}
