7179cef58cef495dac30e08674b83cd9ed21ad19
South <tony.zhangtong@outlook.com>
2017-03-09 16:22:39 -0500

    Memory control method

---
+++ MemoryControl/ExecDemoFini.java
@@ -0,0 +1,16 @@
+package org.totalbeginner;
+
+public class ExecDemoFini {
+	public static void main(String args[]) {
+	Runtime r = Runtime.getRuntime();
+	Process p = null;
+	try {
+		p = r.exec("notepad");
+		p.waitFor();
+	} catch (Exception e) {
+		System.out.println("Error executing notepad.");
+	}
+	System.out.println("Notepad returned " + p.exitValue());
+
+	}
+}
